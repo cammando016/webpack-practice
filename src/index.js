f89dafb327532f20cd698aaf0fe82e2d4da987be
Cameron Anderson <cammando@hotmail.com.au>
2024-08-04 16:48:30 +1000

    Setup project files and webpack config

---
+++ src/index.js
@@ -0,0 +1,3 @@
+export function AlertTest () {
+    alert("Testing");
+}
---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title:"Restaurant Demo",
+        }),
+    ],
+};
+
+
+//NEED HTML TEMPLATE
